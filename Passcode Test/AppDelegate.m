f74b6504940435ba39e14df20317a8b62473e4ef
Daniel Tull <dt@danieltull.co.uk>
2013-02-28 09:28:26 +0000

    Thought the data retrieval would fail

--- Passcode Test/AppDelegate.m
+++ Passcode Test/AppDelegate.m
@@ -8,42 +8,45 @@
 
 #import "AppDelegate.h"
 
-@implementation AppDelegate
+@implementation AppDelegate {
+	NSURL *_fileURL;
+}
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOption {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+
+	NSString *string = @"Test";
+	NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+	NSURL *documentsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
+																  inDomains:NSUserDomainMask] lastObject];
+	_fileURL = [documentsURL URLByAppendingPathComponent:@"test"];
+
+	NSLog(@"%@", _fileURL);
+
+	[data writeToURL:_fileURL options:NSDataWritingFileProtectionComplete error:NULL];
+
+	NSData *data2 = [NSData dataWithContentsOfURL:_fileURL options:NSDataReadingUncached error:NULL];
+	NSString *string2 = [[NSString alloc] initWithData:data2 encoding:NSUTF8StringEncoding];
+
+	NSLog(@"%@ %@", string, string2);
+
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
+- (void)applicationDidEnterBackground:(UIApplication *)application {
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
+	UIBackgroundTaskIdentifier backgroundTaskIdentifier = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:NULL];
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
+	[[NSOperationQueue new] addOperationWithBlock:^{
+		NSData *data = [NSData dataWithContentsOfURL:_fileURL options:NSDataReadingUncached error:NULL];
+		NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+		NSLog(@"%@", string);
+		[[UIApplication sharedApplication] endBackgroundTask:backgroundTaskIdentifier];
+	}];
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
 }
 
 @end
