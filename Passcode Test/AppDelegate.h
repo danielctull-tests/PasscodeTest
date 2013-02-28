184e197e41e8b7395c372dead165129282e5d6e7
Daniel Tull <dt@danieltull.co.uk>
2013-02-28 09:03:55 +0000

    Initial Commit

---
+++ Passcode Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Passcode Test
+//
+//  Created by Daniel Tull on 28.02.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Passcode Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Passcode Test
+//
+//  Created by Daniel Tull on 28.02.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
